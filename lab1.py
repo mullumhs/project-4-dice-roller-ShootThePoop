45c5f912fa2fe671258075b14d07ca00d288b570
ShootThePoop <147568900+ShootThePoop@users.noreply.github.com>
2025-03-04 09:49:37 +1100

    twitzle

--- lab1.py
+++ lab1.py
@@ -6,17 +6,46 @@
 - Class: Software Engineering
 - Description: Create and use functions in Python
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+import random
 
 # Write a function named welcome_message that prints out "Welcome to our program!" whenever it is called.
-
+def welcome_message():
+    print("Welcome to our program!")
 
 # Create a function named print_divider that prints out a line of asterisks (e.g., "**********") to act as a section divider.
-
+def print_divider():
+    print("*"*50)
 
 # Write a function named get_number that asks the user to input a whole number and then returns the result as an integer.
-
+def get_number():
+    a = int(input("Please input a whole number: "))
+    print (a)
 
 # Create a function named get_random_colour that, when called, returns a random colour from a predefined list of colours.
+def get_random_colour():
+    randomcolour = random.randint(1,6)
+
+    if randomcolour == 1:
+        return "Blue"
+
+    elif randomcolour == 2:
+        return "Red"
+        
+    elif randomcolour == 3:
+        return "Yellow"
+        
+    elif randomcolour == 4:
+        return "Orange"
+        
+    elif randomcolour == 5:
+        return "Purple"
+        
+    elif randomcolour == 6:
+        return "Black"
 
 
 # Call all of your functions to demonstrate that they work
+welcome_message()
+print_divider()
+get_number()
+print(get_random_colour())
--- lab2.py
+++ lab2.py
@@ -8,13 +8,19 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 # Create a function named say_hello that accepts a person's name as a parameter and prints "Hello" followed by the name.
-
+def say_hello():
+    name = input("Please enter your name: ")
+    print(f"Hello {name}.")
 
 # Develop a function named triple that takes one number as a parameter and returns the number multiplied by three.
-
+def triple():
+    a = 9
+    b = a * 3
+    print(b)
 
 # Write a function called add that takes two numbers as parameters and returns their sum. 
-
+say_hello()
+triple()
 
 # Create a function named draw_line that takes one parameter for the length of the line and prints a straight line of that many hyphens.
 
