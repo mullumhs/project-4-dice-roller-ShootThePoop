7ce66e7b7c3e6d07d9911d270b60121b24b23451
ShootThePoop <147568900+ShootThePoop@users.noreply.github.com>
2025-03-13 09:50:16 +1100

    lab 2 done lab 3 started

--- lab2.py
+++ lab2.py
@@ -8,21 +8,39 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 # Create a function named say_hello that accepts a person's name as a parameter and prints "Hello" followed by the name.
-def say_hello():
-    name = input("Please enter your name: ")
-    print(f"Hello {name}.")
+def say_hello(name):
+    return(f"Hello {name}.")
+    
 
 # Develop a function named triple that takes one number as a parameter and returns the number multiplied by three.
-def triple():
-    a = 9
-    b = a * 3
-    print(b)
+def triple(x):
+    return(x*3)
 
 # Write a function called add that takes two numbers as parameters and returns their sum. 
-say_hello()
-triple()
+def add(one, two):
+    return(one+two)
 
 # Create a function named draw_line that takes one parameter for the length of the line and prints a straight line of that many hyphens.
+def draw_line(length):
+    return("-"*length)
 
 
 # Call your functions, printing out the return result where appropriate
+
+
+
+# say hello
+print(say_hello("Jenga"))
+
+# triple
+x = 13
+print(triple(x))
+
+# add
+one = 3
+two = 4
+print(add(one,two))
+
+#draw line
+length = 300
+print(draw_line(length))
--- lab3.py
+++ lab3.py
@@ -9,13 +9,27 @@
 
 # Define a function called get_number(prompt) that returns an integer. 
 # Include a while loop and try/except blocks inside the function to handle non-integer inputs.
+def get_number(prompt):
+    while True:
+        try:
+            my_num = int(input(prompt))
+            return my_num
 
+        except:
+            print("invalid number")   
+
+# x = get_number("Please enter a number: ")
 
 
 # Use the get_number function to ask for a numerator, then again for a denominator.
 # Divide the numerator by the denominator and print the result, handling any division by zero errors.
+def fraction():
+    numerator = get_number("Please enter your numerator: ")
+    denominator = get_number("Please enter your denominator: ")
+    print(numerator/denominator)
 
-
-
+fraction()
 # Use the get_number function to ask the user for an index to access an element from a predefined list. 
 # Print out the value from the list, handling the index error if the user selects a non-existent index.
+            
+
