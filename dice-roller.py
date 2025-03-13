5a4a41c003889c55d6d5dc45f2b51a31998a89a4
ShootThePoop <147568900+ShootThePoop@users.noreply.github.com>
2025-03-14 10:39:49 +1100

    dice roller started lab 3 finished

--- dice-roller.py
+++ dice-roller.py
@@ -6,3 +6,68 @@
 - Class: Software Engineering
 - Description: Complete a functional dice roller app in Python
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+import random
+
+#Get number function
+def get_number(prompt):
+    while True:
+        try:
+            my_num = int(input(prompt))
+            return my_num
+
+        except:
+            print("Invalid number.")
+
+
+
+#Find out how many dice they want
+def amountofdice():
+    while True:
+        numofdice = get_number("Please enter how many dice you would like to roll: ")
+        return numofdice
+
+
+#Find out how many sides they want their dice to have
+def howmanysides():
+    while True:
+        numofsides = get_number("Please enter how many sides you would like your dice to have: ")
+        return numofsides
+
+
+
+rolls = 0
+
+def rolling():
+    while rolls < amountofdice:
+        #get rid of break only there for now while oim mamking it
+        # GET RID OF BREAK
+        break
+
+
+
+amountofdice()
+howmanysides()
+
+
+'''
+
+#Establish the rolls function
+rolls = 0
+
+
+#Rolling the dice
+def rollingdice(dice, sides):
+    while rolls < dice():
+        diceresult = random.randint(1, sides)
+        
+        #After each roll, the total rolls increases.
+        #Program will run until rolls is 1 below numofdice function
+        #When rolls = numofdice the rollingdice() def will stop.
+        rolls = rolls + 1
+
+
+dice = amountofdice()
+sides = howmanysides()
+rollingdice(dice, sides)
+
+'''
--- lab3.py
+++ lab3.py
@@ -9,6 +9,7 @@
 
 # Define a function called get_number(prompt) that returns an integer. 
 # Include a while loop and try/except blocks inside the function to handle non-integer inputs.
+
 def get_number(prompt):
     while True:
         try:
@@ -16,20 +17,37 @@ def get_number(prompt):
             return my_num
 
         except:
-            print("invalid number")   
+            print("Invalid number.")   
 
+get_number("Enter a number: ")
 # x = get_number("Please enter a number: ")
 
 
 # Use the get_number function to ask for a numerator, then again for a denominator.
 # Divide the numerator by the denominator and print the result, handling any division by zero errors.
+
 def fraction():
-    numerator = get_number("Please enter your numerator: ")
-    denominator = get_number("Please enter your denominator: ")
-    print(numerator/denominator)
+
+    numerator = get_number("Enter a number for your numerator: ")
+
+    while True:
+        try:
+            denominator = get_number("Enter a number for your denominator: ")
+            answer = numerator / denominator
+            print(answer)
+            break
+        except:
+            print("Cannot divide by zero.")
 
 fraction()
+
 # Use the get_number function to ask the user for an index to access an element from a predefined list. 
 # Print out the value from the list, handling the index error if the user selects a non-existent index.
             
+my_list = ["apples", "bananas", "avocado", "cherries"]
 
+try:
+    item = get_number(f"Enter a number between 0 and {len(my_list)-1} for an item from your shopping list: ")
+    print(my_list[item])
+except:
+    print(f"Enter a number between 0 and {len(my_list)-1}")
